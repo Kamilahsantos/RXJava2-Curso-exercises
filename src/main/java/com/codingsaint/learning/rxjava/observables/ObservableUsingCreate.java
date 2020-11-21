733fef48d1987d576cdb391feff1033a0e08e1ef
Kamila fatima santos oliveira <kamila.fatima@amedigital.com>
2020-11-21 18:29:47 -0300

    creating observables using create

---
+++ src/main/java/com/codingsaint/learning/rxjava/observables/ObservableUsingCreate.java
@@ -0,0 +1,41 @@
+package com.codingsaint.learning.rxjava.observables;
+
+import com.codingsaint.learning.rxjava.models.Shape;
+import com.codingsaint.learning.rxjava.observer.DemoObserver;
+import com.codingsaint.learning.rxjava.utils.RxUtils;
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+public class ObservableUsingCreate {
+
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ObservableUsingCreate.class);
+
+
+    public static void main(String... args) {
+
+        List<Shape> shapes = RxUtils.shapes(15);
+
+        Observable.create(new ObservableOnSubscribe<Object>() {
+            @Override
+            public void subscribe(ObservableEmitter<Object> observableEmitter) throws Exception {
+
+                try {
+
+                    shapes.forEach(observableEmitter::onNext);
+
+                } catch (Exception e) {
+                    observableEmitter.onError(e);
+                }
+
+                observableEmitter.onComplete();
+            }
+        }).subscribe(new DemoObserver());
+
+    }
+}
